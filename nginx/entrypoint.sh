00ee5306070095b434d6928c845f460e7018dc27
morch <70644384+morch1@users.noreply.github.com>
2022-01-23 23:08:12 +0100

    hmm

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 envsubst '${NGINX_DOMAIN}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
-while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g "daemon off;"
+while :; do sleep 6h ; nginx -s reload; done & nginx -g "daemon off;"
 exec "$@"
