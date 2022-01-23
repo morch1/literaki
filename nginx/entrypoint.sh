a9fbdeebc77f44e87e96bf2bb515da385e955901
morch <70644384+morch1@users.noreply.github.com>
2022-01-24 00:16:17 +0100

    make usable with nginx-proxy

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-envsubst '${NGINX_DOMAIN}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
+envsubst '${VIRTUAL_HOST}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 while :; do sleep 6h ; nginx -s reload; done & nginx -g "daemon off;"
 exec "$@"
